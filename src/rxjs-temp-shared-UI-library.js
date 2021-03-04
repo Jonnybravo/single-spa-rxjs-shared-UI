09dc2837053db578ae965b678abe04b46417b6de
Taborda, Joao <Joao.Taborda@pmi.com>
2021-03-04 12:48:58 +0000

    - testing versioning;

--- src/rxjs-temp-shared-UI-library.js
+++ src/rxjs-temp-shared-UI-library.js
@@ -1,12 +1,5 @@
 import "./global.css?modules=false";
-// import {teste} from "./button.component.js";
 
-// export {default as J} from "./button2.component.js";;
-
-// You can also export React components from this file and import them into your microfrontends
-// export { default as teste } from "./button.component.js";
-
-// export { default as Button } from "./button.component.js";
-export { default as Input } from "./Input/Input";
 export { default as Title } from "./Typography/Title";
-export { default as Button } from "./Button/Button";
+// export { default as Input } from "./Input/Input";
+// export { default as Button } from "./Button/Button";
