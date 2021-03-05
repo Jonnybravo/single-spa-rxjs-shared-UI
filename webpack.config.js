c266cce62fb2ad71727338c32f1e04683dd72908
Taborda, Joao <Joao.Taborda@pmi.com>
2021-03-05 19:11:17 +0000

    - no single-spa

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
-const { merge } = require("webpack-merge");
+// const { merge } = require("webpack-merge");
 const path = require("path");
-const singleSpaDefaults = require("webpack-config-single-spa");
+// const singleSpaDefaults = require("webpack-config-single-spa");
 
-module.exports = (webpackConfigEnv, argv) => {
-  const defaultConfig = singleSpaDefaults({
-    orgName: "rxjs-temp",
-    projectName: "shared-UI-library",
-    webpackConfigEnv,
-    argv,
-  });
+// (webpackConfigEnv, argv) => {
+//   const defaultConfig = singleSpaDefaults({
+//     orgName: "rxjs-temp",
+//     projectName: "shared-UI-library",
+//     webpackConfigEnv,
+//     argv,
+//   });
 
-  return merge(defaultConfig, {
-    mode: "production",
-    entry: "./src/index.js",
-    output: {
-      path: path.join(__dirname, "build"),
-      filename: "index.js",
-      libraryTarget: "commonjs2",
-      publicPath: "/build/",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js?$/,
-          exclude: /(node_modules)/,
-          use: "babel-loader",
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".js"],
-    },
-  });
+//   return merge(defaultConfig,
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+    publicPath: "/build/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  // });
 };
